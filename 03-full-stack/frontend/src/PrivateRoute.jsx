432c9f0865d86db11128b8565f2244d3e3e4015d
afsaanchez04 <afsanchez04@gmail.com>
2026-06-08 22:08:09 -0500

    implementación AuthContext en PrivateRoute.jsx

--- 03-full-stack/frontend/src/PrivateRoute.jsx
+++ 03-full-stack/frontend/src/PrivateRoute.jsx
@@ -1,29 +1,11 @@
-import { useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
-import { api } from './api'
+import { useAuth } from './context/AuthContext'
 
 export const PrivateRoute = ({children}) => {
 
-  const [auth, setAuth] = useState(null)
+  const { user, loading } = useAuth()
 
-  useEffect(() => {
-    
-    const checkAuth = async () => {
-      try {
-        await api.get("/profile")
-        setAuth(true)
-      } catch (error) {
-        console.log(error)
-        setAuth(false)
-      }
-    }
-
-    checkAuth()
-    
-  }, [])
-
-  if( auth === null ) return <p>Cargando...</p>
+  if( loading ) return <p>Cargando...</p>
   
-
-  return auth ? children : <Navigate to="/login"/>
+  return user ? children : <Navigate to="/login"/>
 }
