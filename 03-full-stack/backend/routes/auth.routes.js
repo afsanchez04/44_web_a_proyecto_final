6bd655d99698efd9f3852b3b82705877e27fdb1d
afsaanchez04 <afsanchez04@gmail.com>
2026-06-08 20:52:38 -0500

    implementación de validaciones en auth.routes.js

--- 03-full-stack/backend/routes/auth.routes.js
+++ 03-full-stack/backend/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import { login, logout, profile, register } from '../controllers/auth.controller.js'
 import { authRequired } from '../middlewares/validateToken.js'
+import { loginValidator, registerValidator } from '../validators/auth.validator.js'
+import { handleValidator } from '../middlewares/handleValidator.js'
+
 
 const router = Router()
 
-router.post( '/register', register )
-router.post( '/login', login )
+router.post( '/register', registerValidator, handleValidator, register )
+router.post( '/login', loginValidator, handleValidator, login )
 router.post( '/logout', logout )
 router.get( '/profile', authRequired, profile )
 
